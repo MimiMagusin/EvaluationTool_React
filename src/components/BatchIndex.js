a3b0bef938159729397f97ce83dc740a213f2bb2
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-10 14:58:23 +0200

    Change root route, create Batch Index, install chai

---
+++ src/components/BatchIndex.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+
+class BatchIndex extends PureComponent {
+  render(){
+    return(
+      <p> batches coming soon </p>
+    )
+  }
+}
+
+export default BatchIndex
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import registerServiceWorker from './registerServiceWorker'
 
 import App from './App'
 import signIn from './components/users/sign-in'
+import BatchIndex from './components/BatchIndex'
 
 import './index.css'
 

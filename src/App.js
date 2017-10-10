74a2ee9c29fc6678c64d3c8067c555ca9d3ea99b
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-10 17:31:26 +0200

    Create Title Component, install prop-types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Title from './components/Title'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        < Title content= "Evaluate" />
         { this.props.children }
       </div>
     )

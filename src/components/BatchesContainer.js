baaf72be067a3b49c96d2c6574794e963aae4a97
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-11 10:22:24 +0200

    This time saved

--- src/components/BatchesContainer.js
+++ src/components/BatchesContainer.js
@@ -1,24 +1,46 @@
+// src/recipes/RecipesContainer.js
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import Title from '../components/Title'
+import BatchItem from './BatchItem'
+//import BatchEditor from './BatcheEditor'
+import fetchBatches from '../actions/batches/fetch'
+import subscribeToBatchesService from '../actions/batches/subscribe'
+//import './BatchesContainer.css'
 
-class BatchesContainer extends PureComponent {
+export class BatchesContainer extends PureComponent {
+  static propTypes = {
+    batches: PropTypes.array.isRequired,
+    fetchBatches: PropTypes.func.isRequired,
+  }
+
+  componentWillMount() {
+    this.props.fetchBatches()
+    this.props.subscribeToBatchesService()
+  }
+
   renderBatch(batch, index) {
-    return null // this will come later
+    return <BatchItem
+      key={index} { ...batch } />
   }
 
   render() {
     return(
       <div className="batches wrapper">
         <header>
-          <Title content="Batches" />
+          <Title content=" All Batches" />
         </header>
 
         <main>
-            { this.props.batches.map(this.renderBatch) }
+          { this.props.batches.map(this.renderBatch.bind(this)) }
         </main>
       </div>
     )
   }
 }
 
-export default BatchesContainer
+const mapStateToProps = ({ batches }) => ({ batches })
+const mapDispatchToProps = { fetchBatches, subscribeToBatchesService }
+
+export default connect(mapStateToProps, mapDispatchToProps)(BatchesContainer)
